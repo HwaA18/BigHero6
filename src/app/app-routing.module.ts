b12ea96c4a6322cdc2d72cfd78ebfb80fb868979
hwaa18 <austinhwa@MacBook-Pro.fios-router.home>
2020-07-31 17:12:10 -0400

    deployables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CommentsComponent } from './comments/comments.component';
+  
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'comments', component: CommentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
